c09689490f8adaa74578731d0aac9601ba505380
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-27 09:00:11 -0600

    Initial commit

---
+++ src/test/java/com/example/moviereservationsystem/MoviereservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
